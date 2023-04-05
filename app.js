3aa564421e92e20ff2642ae7751a4d3efb9fd459
mauriau <mauriau.pro@proton.me>
2023-04-05 19:10:16 +0200

    feat: add route api/stuff with data

--- app.js
+++ app.js
@@ -13,14 +13,26 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use((req, res, next) => {
-    res.json({message: 'Hello World with express !'})
-    next();
+app.use('/api/stuff', (req, res, next) => {
+    const stuff = [
+        {
+            _id: 'oeihfzeoi',
+            title: 'Mon premier objet',
+            description: 'Les infos de mon premier objet',
+            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+            price: 4900,
+            userId: 'qsomihvqios',
+        },
+        {
+            _id: 'oeihfzeomoihi',
+            title: 'Mon deuxième objet',
+            description: 'Les infos de mon deuxième objet',
+            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+            price: 2900,
+            userId: 'qsomihvqios',
+        },
+    ];
+    res.status(200).json(stuff);
 });
-
-app.use((req, res) => {
-    console.log('Réponse envoyée avec succès !');
-});
-
 
 module.exports = app;
