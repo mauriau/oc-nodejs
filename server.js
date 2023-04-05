4a40ecf69869d3b8285b15389b81d8658398743f
mauriau <mauriau.pro@proton.me>
2023-04-05 18:46:44 +0200

    feat: server is more functionnable

--- server.js
+++ server.js
@@ -1,8 +1,41 @@
 const http = require('http');
 const app = require('./app');
-const port = process.env.PORT || 3000;
-app.set('port', port)
+
+const normalizePort = val => {
+    const port =  parseInt(val, 10);
+    return isNaN(port) ? val : port >= 0 ? port : false;
+}
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+const errorHandler = error => {
+    if(error.syscall !== 'listen') {
+        throw error;
+    }
+
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges.');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use.');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+};
 
 const server = http.createServer(app);
 
-server.listen(process.env.PORT || 3000);
+server.on('error', errorHandler);
+server.on('listening', () => {
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+    console.log('Listening on ' + bind);
+});
+
+server.listen(port);
