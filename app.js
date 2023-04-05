10157d30a70abfd06cad6a15356d437aefe4abe6
mauriau <mauriau.pro@proton.me>
2023-04-05 17:24:42 +0200

    feat: use middleware

--- app.js
+++ app.js
@@ -2,10 +2,25 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res) => {
-    res.json({message: 'Hello World with express !'})
+
+app.use((req, res, next) => {
+    console.log('Requête reçue');
+    next();
 });
 
+app.use((req, res, next) => {
+    res.status(201);
+    next();
+});
+
+app.use((req, res, next) => {
+    res.json({message: 'Hello World with express !'})
+    next();
+});
+
+app.use((req, res) => {
+    console.log('Réponse envoyée avec succès !');
+});
 
 
 module.exports = app;
