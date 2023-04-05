12e97c98e5a89e29090903decd0ba9f2d045628b
mauriau <mauriau.pro@proton.me>
2023-04-05 19:10:34 +0200

    feat: allow CORS

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express = require('express');
 
 const app = express();
 
-
+// add CORS
 app.use((req, res, next) => {
-    console.log('Requête reçue');
-    next();
-});
-
-app.use((req, res, next) => {
-    res.status(201);
+    // Allow access to API
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Allow some headers on request
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    // Allow some methods
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
 
