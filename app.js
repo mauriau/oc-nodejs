c5f43837fa708be4f2513e48cfc80ff717eab4ab
mauriau <mauriau.pro@proton.me>
2023-04-06 17:46:03 +0200

    feat: cand delete a thing

--- app.js
+++ app.js
@@ -50,6 +50,12 @@ app.put('/api/stuff/:id', (req, res, next) => {
         .catch(error => res.status(400).json({error}));
 });
 
+app.delete('/api/stuff/:id', (req, res, next) => {
+    Thing.deleteOne({_id: req.params.id})
+    .then(() => res.status(200).json({message: 'objet supprimé !'}))
+    .catch(error => res.status(400).json({error}))
+})
+
 
 app.get('/api/stuff/:id', (req, res, next) => {
     Thing.findOne({
