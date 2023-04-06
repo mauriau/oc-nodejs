8b3f7c969497e36f0eeaf0629a9ff7e5dca88e1e
mauriau <mauriau.pro@proton.me>
2023-04-06 17:41:48 +0200

    feat: can update a thing

--- app.js
+++ app.js
@@ -41,8 +41,17 @@ app.post('/api/stuff', (req, res, next) => {
     ;
 })
 
+app.put('/api/stuff/:id', (req, res, next) => {
+    Thing.updateOne({
+        _id: req.params.id,
+
+    }, {...req.body, _id: req.params.id})
+        .then(thing => res.status(200).json({message: 'objet modifié'}))
+        .catch(error => res.status(400).json({error}));
+});
+
+
 app.get('/api/stuff/:id', (req, res, next) => {
-    console.log('id',req.params.id);
     Thing.findOne({
         _id: req.params.id
     })
