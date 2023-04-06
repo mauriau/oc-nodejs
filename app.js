1f5ff83c4723e7b1a5bcaecb15a3f6520a86bb16
mauriau <mauriau.pro@proton.me>
2023-04-06 18:17:18 +0200

    feat: create api for product (quiz)

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 const app = express();
 
 const Thing = require('./models/Thing');
+const Product = require('./models/Product');
 
 dotenv.config();
 
@@ -68,6 +69,50 @@ app.get('/api/stuff/:id', (req, res, next) => {
 app.get('/api/stuff', (req, res, next) => {
     Thing.find()
         .then(things => res.status(200).json(things))
+        .catch(error => res.status(400).json({error}))
+});
+
+
+// POST request must be before get request, because get catch the request before post
+app.post('/api/products', (req, res, next) => {
+    const product = new Product({
+        ...req.body
+    });
+    product.save()
+        .then(() => res.status(201).json({product}))
+        .catch(error => res.status(400).json({error}))
+    ;
+})
+
+app.put('/api/products/:id', (req, res, next) => {
+    Product.updateOne({
+        _id: req.params.id,
+
+    }, {...req.body, _id: req.params.id})
+        .then(product => res.status(200).json({ message: 'Modified!' }))
+        .catch(error => res.status(400).json({error}));
+});
+
+app.delete('/api/products/:id', (req, res, next) => {
+    Product.deleteOne({_id: req.params.id})
+    .then(() => res.status(200).json({ message: 'Deleted!' }))
+    .catch(error => res.status(400).json({error}))
+})
+
+
+app.get('/api/products/:id', (req, res, next) => {
+    Product.findOne({
+        _id: req.params.id
+    })
+    .then((product) =>
+        res.status(200).json({product})
+    )
+    .catch(error => res.status(404).json({error}));
+})
+
+app.get('/api/products', (req, res, next) => {
+    Product.find()
+        .then(products => res.status(200).json({products}))
         .catch(error => res.status(400).json({error}))
 });
 
