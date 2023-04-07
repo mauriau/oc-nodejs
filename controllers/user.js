7d116d4c66685e7afe6c821e3b4f8631e7720da1
mauriau <mauriau.pro@proton.me>
2023-04-07 11:15:53 +0200

    feat: user can login

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,7 @@
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+dotenv.config();
+
 exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10)
         .then(hash => {
@@ -12,9 +16,28 @@ exports.signup = (req, res, next) => {
         .catch(error => res.status(500).json({ error }));
 };
 
-}
-
-exports.login = (req, res, next)=> {
-
-}
+exports.login = (req, res, next) => {
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if (!user) {
+                return res.status(401).json({ message: 'Paire login/mot de passe incorrecte'});
+            }
+            bcrypt.compare(req.body.password, user.password)
+                .then(valid => {
+                    if (!valid) {
+                        return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });
+                    }
+                    res.status(200).json({
+                        userId: user._id,
+                        token: jwt.sign(
+                            { userId: user._id },
+                            process.env.TOKEN,
+                            {expiresIn: '24h'}
+                        )
+                    });
+                })
+                .catch(error => res.status(500).json({ error }));
+        })
+        .catch(error => res.status(500).json({ error }));
+};
 
--- routes/stuff.js
+++ routes/stuff.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
+const auth = require('../middleware/auth');
+
 const stuffController = require('../controllers/stuff');
 
-router.post('/', stuffController.createThing)
+router.post('/', auth, stuffController.createThing)
 
-router.put('/:id', stuffController.updateThing);
+router.put('/:id', auth, stuffController.updateThing);
 
-router.delete('/:id', stuffController.deleteThing);
+router.delete('/:id', auth, stuffController.deleteThing);
 
-router.get('/:id', stuffController.getOneThing)
+router.get('/:id',auth, stuffController.getOneThing);
 
-router.get('/', stuffController.getAllThings);
+router.get('/',auth, stuffController.getAllThings);
 
 module.exports = router;
